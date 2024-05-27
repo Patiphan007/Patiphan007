30952e44e794ca2b28444e1bff30e9cf02b5d1a5
Patience <mtn.oppo59913@gmail.com>
2024-05-27 15:09:17 +0700

    first commit

---
+++ app1.py
@@ -0,0 +1,4 @@
+sayHi = 'welcome to python'
+name = 'mr .siu'
+age = 17
+print(f'{sayHi} name: {name}  Age: {age}')
---
+++ app2.py
@@ -0,0 +1,9 @@
+inter_nember = 123 #integer
+flat_number = 1.23 #float
+
+#implicit type conveerrsion
+new_number = inter_nember + flat_number
+#display
+
+print(F'Value: {new_number}') #124.23
+print(f'Data Type: type:', (new_number))
